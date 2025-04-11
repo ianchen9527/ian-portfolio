f5dd07f27a13c92163f101da96bc635d36c5574b
Ian Chen <ianchen@IandeMacBook-Pro.local>
2025-04-11 15:16:08 +0800

    feat: create empty about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <main className="p-10">
+      <h1 className="text-9xl font-bold">關於我</h1>
+      <p className="text-6xl mt-4">這是我的個人介紹頁面。</p>
+    </main>
+  )
+}
