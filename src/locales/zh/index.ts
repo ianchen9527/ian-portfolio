d134fe18165c5989492948646aaf763e4ec24a21
Ian Chen <ianchen@IandeMacBook-Pro.local>
2025-04-17 14:27:20 +0800

    feat(i18n): create locale for replica-showcase overview page

--- src/locales/zh/index.ts
+++ src/locales/zh/index.ts
@@ -1,11 +1,13 @@
 import home from "./home.json"
 import about from "./about.json"
 import projects from "./projects.json"
+import projectsReplicaShowcase from "./projects/replica-showcase.json"
 
 const zh = {
   home,
   about,
   projects,
+  "projects/replica-showcase": projectsReplicaShowcase,
 }
 
 export default zh
