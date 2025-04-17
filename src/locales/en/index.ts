26714e5d05b1ee8020a6f52ae348f442d1359da9
Ian Chen <ianchen@IandeMacBook-Pro.local>
2025-04-17 15:35:10 +0800

    feat(i18n): create locale for replica-showcase/paul-archer-design overview page

--- src/locales/en/index.ts
+++ src/locales/en/index.ts
@@ -1,13 +1,16 @@
 import home from "./home.json"
 import about from "./about.json"
 import projects from "./projects.json"
-import projectsReplicaShowcase from "./projects/replica-showcase.json"
+import projectsReplicaShowcase from "./projects/replica-showcase/index.json"
+import projectsReplicaShowcasePaulArcherDesign from "./projects/replica-showcase/paul-archer-design.json"
 
 const en = {
   home,
   about,
   projects,
   "projects/replica-showcase": projectsReplicaShowcase,
+  "projects/replica-showcase/paul-archer-design":
+    projectsReplicaShowcasePaulArcherDesign,
 }
 
 export default en
