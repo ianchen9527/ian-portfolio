ce203d36e7f77e89dd8f9c6d23e2864ae02896cf
Ian Chen <ianchen@IandeMacBook-Pro.local>
2025-04-17 12:47:17 +0800

    feat(i18n): create projects page locale

--- src/locales/en/index.ts
+++ src/locales/en/index.ts
@@ -1,9 +1,11 @@
 import home from "./home.json"
 import about from "./about.json"
+import projects from "./projects.json"
 
 const en = {
   home,
   about,
+  projects,
 }
 
 export default en
--- src/locales/zh/index.ts
+++ src/locales/zh/index.ts
@@ -1,9 +1,11 @@
 import home from "./home.json"
 import about from "./about.json"
+import projects from "./projects.json"
 
 const zh = {
   home,
   about,
+  projects,
 }
 
 export default zh
